1c4a3102208d499ce70b9af859cc8c5fa9605cfc
rejupillai <reju.pillai@gmail.com>
2016-07-19 16:38:06 +0530

    Added 1.0 version of images

--- machines/create-hosts.cmd
+++ machines/create-hosts.cmd
@@ -1,35 +1,43 @@
+
+
+@REM - Removes all the dockerhosts before creating new nodes
+
+docker-machine rm dockerhost-keystore dockerhost-swarm-master  dockerhost-registry dockerhost-application dockerhost-ci --force
+
+
 @REM - Create a node for Keystore to be used by Consul  
-docker-machine create -d virtualbox DOCKERHOST-KEYSTORE
-docker-machine env DOCKERHOST-KEYSTORE
+docker-machine create -d virtualbox dockerhost-keystore
+docker-machine env dockerhost-keystore
+ @FOR /f "tokens=*" %i IN ('docker-machine env dockerhost-keystore') DO @%i
 docker run -d -p "8500:8500" -h "consul" progrium/consul -server -bootstrap
 
 @REM - Get the generated IP-Addr for the Consul's Keystore
-SET DOCKERHOST-KEYSTORE-IP-CMD="docker-machine ip DOCKERHOST-KEYSTORE"
-FOR /F %i IN ( '%DOCKERHOST-KEYSTORE-IP-CMD%' ) DO  SET DOCKERHOST-KEYSTORE-IP=%i
+SET dockerhost-keystore-ip-cmd="docker-machine ip dockerhost-keystore"
+@FOR /F %i IN ( '%dockerhost-keystore-ip-cmd%' ) DO  SET   dockerhost-keystore-ip=%i
 
 @REM - Create a Swarm master node pointing to the the Consul address
-docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery="consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-store=consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-advertise=eth1:2376" DOCKERHOST-SWARMMASTER
-
-@REM - Create a Swarm worker node for Storing all data . This node will host data-containers
-docker-machine create -d virtualbox --virtualbox-memory "3072" --swarm --swarm-discovery="consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-store=consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-advertise=eth1:2376" DOCKERHOST-STORAGE
+docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery="consul://%dockerhost-keystore-ip%:8500" --engine-label nodename=dh-swarm-master --engine-opt="cluster-store=consul://%dockerhost-keystore-ip%:8500" --engine-opt="cluster-advertise=eth1:2376" dockerhost-swarm-master
 
 @REM - Create a Swarm worker node for Docker local Registry to store images within the Firewall of the organizations
-docker-machine create -d virtualbox --virtualbox-memory "1024" --swarm --swarm-discovery="consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-store=consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-advertise=eth1:2376" DOCKERHOST-REGISTRY
+docker-machine create -d virtualbox --virtualbox-memory "1024" --swarm --swarm-discovery="consul://%dockerhost-keystore-ip%:8500" --engine-label nodename=dh-registry  --engine-insecure-registry localregistry:5000 --engine-opt="cluster-store=consul://%dockerhost-keystore-ip%:8500" --engine-opt="cluster-advertise=eth1:2376" dockerhost-registry
 
 @REM - Create a Swarm worker node for hosting Applications 
-docker-machine create -d virtualbox --virtualbox-memory "3072" --swarm --swarm-discovery="consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-store=consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-advertise=eth1:2376" DOCKERHOST-APP
+@REM docker-machine create -d virtualbox --virtualbox-memory "3072" --swarm --swarm-discovery="consul://%dockerhost-keystore-ip%:8500" --engine-label="nodename=dh-application" --engine-opt="cluster-store=consul://%dockerhost-keystore-ip%:8500" --engine-opt="cluster-advertise=eth1:2376" dockerhost-application
 
 @REM - Create a Swarm worker node for hosting Applications 
-docker-machine create -d virtualbox --virtualbox-memory "2048" --swarm --swarm-discovery="consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-store=consul://%DOCKERHOST-KEYSTORE-IP%:8500" --engine-opt="cluster-advertise=eth1:2376" DOCKERHOST-CI
-
+@REM docker-machine create -d virtualbox --virtualbox-memory "2048" --swarm --swarm-discovery="consul://%dockerhost-keystore-ip%:8500" --engine-label="nodename=dh-ci" --engine-opt="cluster-store=consul://%dockerhost-keystore-ip%:8500" --engine-opt="cluster-advertise=eth1:2376" dockerhost-ci
 
 @REM - Create an overlay network for multi-host communication using 'overlay' driver
-docker-machine env --swarm DOCKERHOST-SWARMMASTER
-@FOR /f "tokens=*" %i IN ('docker-machine env --swarm DOCKERHOST-SWARMMASTER') DO @%i
-docker network create --driver overlay --subnet=10.0.9.0/24 OVERLAY-NETWORK
+docker-machine env --swarm dockerhost-swarm-master
+@FOR /f "tokens=*" %i IN ('docker-machine env --swarm dockerhost-swarm-master') DO @%i
 
 @REM - Lsit all the nodes
 docker-machine ls
 
 @REM - List all networks
-docker-machine network ls
+docker network ls
+
+@REM - Docker Compose up - build from Dockerfile and spin up containers, networks, storage etc
+docker-compose -f compose/docker-compose.yml  up -d 
+
+
